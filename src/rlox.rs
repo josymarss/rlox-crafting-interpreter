6d10644daabce674733f2346d0ff575b75b7e162
josymarss <sylvajosymar@gmail.com>
2024-03-13 19:21:46 +0100

    Some stages

--- src/rlox.rs
+++ src/rlox.rs
@@ -1,38 +1,74 @@
+use core::panic;
 use std::env::args;
 use std::env::args_os;
-use std::file;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
 use std::io::Read;
 use std::process::exit;
 
-pub struct Lox {
 
-}
+// My imports 
+
+mod token;
+pub struct Lox {}
 
 impl Lox {
+    pub fn run_file(arg: String) {
+      let file = File::open(arg);
+      match file {
+        Ok(file) => {
+          let mut bytes = BufReader::new(file);
+          let mut buff = String::new();
+          let result = bytes.read_to_string(&mut buff);
 
-  pub fn run_file(arg:Option<String>) {
-    let bytes = BufReader::new(File::open(arg()?));
-    run(bytes, )
-  }
-
-  pub fn run_prompt() {}
-
-  pub fn run() {
-
-  }
-
-  pub fn main_entry() {
-    if args().len() > 1 {
-      println!("Usage: jlox [script]");
-      exit(64);
+          Self::run(&mut buff);
+        },
+        Err(err) => println!("{}", err),
+      }
+        // 
+        // print!("{:?}",file);
     }
-    if args().len() == 1 {
-      Lox::run_file(args().nth(0));
-    }else {
-      Lox::run_prompt();
-    } 
+
+    pub fn run_prompt() {
+      let mut line: String = String::new(); 
+      let result = io::stdin().read_line(&mut line);
+
+      // let buff = BufReader::new(line);
+
+      loop { 
+        // let buf = BufReader::new(line);
+        match result {
+          Ok(value) => println!("{}", line),
+          Error => panic!("Something wrong!"),
+        }
+        
+        Self::run(&line);
+      }
+        
+    }
+
+    pub fn run(source: &String) {
+      let mut reader;
+      let tokens = io::stdin().read_line(&mut reader);
+    
+      // For now, just print the tokens.
+      for token in tokens.iter() {
+        println!("{}",token);
+      }
+         
+    }
+
+    pub fn main_entry() {
+        if args_os().len() > 1 {
+            println!("Usage: jlox [script]");
+            exit(64);
+        }
+        if args_os().len() == 1 {
+            let path = args().nth(0).expect("Some error occured!");
+            Lox::run_file(path);
+        } else {
+            Lox::run_prompt();
+        }
+    }
   }
-}
